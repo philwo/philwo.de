5ad800b1dc7a39ec185abd6a9deb4d6fc17a962d
Philipp Wollermann <philipp.wollermann@gmail.com>
2012-01-27 11:17:43 +0900

    Ignore socket files in git, fix several issues in fabfile

--- fabfile.py
+++ fabfile.py
@@ -11,16 +11,15 @@ import string
 # available fabric.* functions are listed here:
 # https://github.com/fabric/fabric/blob/master/fabric/api.py
 from fabric.state import env
-from fabric.operations import run, local
+from fabric.operations import run, local, sudo
 from fabric.context_managers import prefix, cd, lcd
-#from fabric.utils import abort
+from fabric.utils import abort
 from fabric.contrib.files import sed
 
 from local_settings import DATABASES
 
 # abspath() will ensure, that there is no trailing slash at the end of the path
-PROJECT_PATH = os.path.abspath(os.path.split(__file__)[0])
-REMOTE_PROJECT_PATH = '/home/philwo/www/philwo.de/philwo/'
+LOCAL_PROJECT_PATH = os.path.abspath(os.path.split(__file__)[0])
 
 DB_NAME = 'philwo_blog'
 DB_USERNAME = 'philwo_blog'
@@ -32,17 +31,21 @@ SERVER_HOSTNAME = 'rory.philwo.de'
 SERVER_PATH = '~/www/philwo.de/philwo'
 SERVER_RSYNCURL = '%s@%s:%s' % (SERVER_USERNAME, SERVER_HOSTNAME, SERVER_PATH,)
 
-env.hosts = [SERVER_HOSTNAME]
+
+# Environments
+def dev():
+    env.hosts = ['localhost']
+    env.project_path = LOCAL_PROJECT_PATH
 
 
-def runserver():
-    with lcd(PROJECT_PATH):
-        with prefix("source deploy/bin/activate"):
-            local("./manage.py runserver")
+def prod():
+    env.hosts = [SERVER_HOSTNAME]
+    env.project_path = '/home/philwo/www/philwo.de/philwo/'
 
 
+# Migrations
 def migrate_initial():
-    with lcd(PROJECT_PATH):
+    with lcd(LOCAL_PROJECT_PATH):
         with prefix("source deploy/bin/activate"):
             local("rm -f base/migrations/????_*")
             local("rm -f articles/migrations/????_*")
@@ -53,88 +56,103 @@ def migrate_initial():
 
 
 def migrate_auto():
-    with lcd(PROJECT_PATH):
+    with lcd(LOCAL_PROJECT_PATH):
         with prefix("source deploy/bin/activate"):
             local("./manage.py schemamigration base --auto || true")
             local("./manage.py schemamigration articles --auto || true")
             local("./manage.py schemamigration photos --auto || true")
 
 
-def reset():
-    with lcd(PROJECT_PATH):
+def bootstrap():
+    with cd(env.project_path):
+        make_venv()
         with prefix("source deploy/bin/activate"):
-            local("psql -d template1 -c \"DROP DATABASE IF EXISTS %s;\"" % (DB_NAME,))
-            local("psql -d template1 -c \"DROP ROLE IF EXISTS %s;\"" % (DB_USERNAME,))
-            local("psql -d template1 -c \"CREATE ROLE %s NOSUPERUSER NOCREATEDB NOCREATEROLE NOINHERIT LOGIN ENCRYPTED PASSWORD '%s';\"" % (DB_USERNAME, DB_PASSWORD,))
-            local("psql -d template1 -c \"CREATE DATABASE %s WITH OWNER=%s;\"" % (DB_NAME, DB_USERNAME,))
-            local("psql -d %s -c \"GRANT ALL ON SCHEMA public TO %s WITH GRANT OPTION;\"" % (DB_NAME, DB_USERNAME,))
+            setup_db()
             local("rm -rf static")
             local("mkdir static")
             local("./manage.py clean_pyc")
+            local("./manage.py collectstatic -v0 --noinput")
             local("./manage.py syncdb --noinput")
             local("./manage.py createcachetable cache")
             local("./manage.py migrate")
             local("./manage.py createsuperuser --username=%s --email=%s" % (USER_USERNAME, USER_MAIL,))
-            local("./manage.py collectstatic -v0 --noinput")
 
 
+# virtualenv management
 def make_venv():
-    with cd(REMOTE_PROJECT_PATH):
-        #if "VIRTUAL_ENV" not in os.environ:
-        #    abort("$VIRTUAL_ENV not found.\n\n")
-        #virtualenv = os.environ["VIRTUAL_ENV"]
-        virtualenv = os.path.join(REMOTE_PROJECT_PATH, 'deploy')
+    with cd(env.project_path):
+        virtualenv = os.path.join(env.project_path, 'deploy')
         run("virtualenv2 --clear --no-site-packages --distribute %s" % (virtualenv,))
         run("pip-2.7 install -E %s --requirement requirements.txt" % (virtualenv,))
 
 
 def update_venv():
-    with cd(REMOTE_PROJECT_PATH):
-        #if "VIRTUAL_ENV" not in os.environ:
-        #    abort("$VIRTUAL_ENV not found.\n\n")
-        #virtualenv = os.environ["VIRTUAL_ENV"]
-        virtualenv = os.path.join(REMOTE_PROJECT_PATH, 'deploy')
+    with cd(env.project_path):
+        virtualenv = os.path.join(env.project_path, 'deploy')
         run("pip-2.7 install -E %s --requirement requirements.txt" % (virtualenv,))
 
 
-def new_secretkey():
-    with cd(REMOTE_PROJECT_PATH):
-        secretkey = ''.join(random.choice(string.letters + string.digits) for i in xrange(50))
-        sed('settings.py', '^SECRET_KEY.*', 'SECRET_KEY = "%s"' % (secretkey,))
-        run("rm settings.py.bak")
-
-
-def download_media():
-    with cd(REMOTE_PROJECT_PATH):
-        run('rsync -av -e ssh --delete %s/media/ media/' % (SERVER_RSYNCURL,))
-
-
-def deploy():
-    local("rsync -av -e ssh --delete --exclude '/deploy/**' --exclude '/static/**' --exclude '*.pyc' %s/ %s/" % (PROJECT_PATH, SERVER_RSYNCURL,))
+# Database management
+def setup_db():
+    with cd(env.project_path):
+        run('psql -d template1 -c "DROP DATABASE IF EXISTS %s"' % (DB_NAME,))
+        run('psql -d template1 -c "DROP ROLE IF EXISTS %s"' % (DB_USERNAME,))
+        run('psql -d template1 -c "CREATE ROLE %s NOSUPERUSER NOCREATEDB NOCREATEROLE NOINHERIT LOGIN ENCRYPTED PASSWORD \'%s\'"' % (DB_USERNAME, DB_PASSWORD,))
+        run('psql -d template1 -c "CREATE DATABASE %s WITH OWNER=%s"' % (DB_NAME, DB_USERNAME,))
+        run('psql -d template1 -c "GRANT ALL ON DATABASE %s TO %s WITH GRANT OPTION"' % (DB_NAME, DB_USERNAME,))
+        run('psql -d %s -c "GRANT ALL ON SCHEMA public TO %s WITH GRANT OPTION"' % (DB_NAME, DB_USERNAME,))
+        run('psql -d %s -c "DROP EXTENSION IF EXISTS plpgsql"' % (DB_NAME,))
 
 
 def backup_db():
-    with cd(REMOTE_PROJECT_PATH):
+    with cd(env.project_path):
         run("pg_dump -Fc -Z9 --serializable-deferrable %s > %s.pg" % (DB_NAME, DB_NAME,))
 
 
 def restore_db():
-    with cd(REMOTE_PROJECT_PATH):
-        run("psql -d template1 -c \"DROP DATABASE IF EXISTS %s;\"" % (DB_NAME,))
-        run("psql -d template1 -c \"DROP ROLE IF EXISTS %s;\"" % (DB_USERNAME,))
-        run("psql -d template1 -c \"CREATE ROLE %s NOSUPERUSER NOCREATEDB NOCREATEROLE NOINHERIT LOGIN ENCRYPTED PASSWORD '%s';\"" % (DB_USERNAME, DB_PASSWORD,))
-        run("psql -d template1 -c \"CREATE DATABASE %s WITH OWNER=%s;\"" % (DB_NAME, DB_USERNAME,))
-        run("pg_restore -O -d %s %s.pg" % (DB_NAME, DB_NAME,))
+    setup_db()
+    with cd(env.project_path):
+        run("pg_restore -U%s -O -x -d %s %s.pg" % (DB_USERNAME, DB_NAME, DB_NAME,))
+
+
+# Deployment
+def deploy():
+    local("rsync -av -e ssh --delete --exclude '/deploy/**' --exclude '/static/**' --exclude '*.pyc' %s/ %s/" % (LOCAL_PROJECT_PATH, SERVER_RSYNCURL,))
+    sudo("supervisorctl restart gunicorn")
+
+
+# Data transfer between dev and prod
+def download_media():
+    with lcd(LOCAL_PROJECT_PATH):
+        local('rsync -av -e ssh --delete %s/media/ media/' % (SERVER_RSYNCURL,))
 
 
 def pull_db():
-    local("ssh %s@%s pg_dump -Fc -Z9 --serializable-deferrable %s | pg_restore -Uphilwo -O -d %s" % (DB_NAME, SERVER_USERNAME, SERVER_HOSTNAME, DB_NAME,))
+    local("for table in `psql %s -c'\dt' -A | grep ^public | cut -d'|' -f2`; do psql %s -c\"drop table if exists $table cascade\"; done" % (DB_NAME, DB_NAME,))
+    local("ssh %s@%s pg_dump -U%s -Fc -Z9 %s | pg_restore -U%s -O -x -d %s" % (SERVER_USERNAME, SERVER_HOSTNAME, DB_USERNAME, DB_NAME, DB_USERNAME, DB_NAME,))
 
 
 def push_db():
-    local("pg_dump -Fc -Z9 --serializable-deferrable %s | ssh %s@%s pg_restore -Uphilwo -O -d %s" % (DB_NAME, SERVER_USERNAME, SERVER_HOSTNAME, DB_NAME,))
+    if 'localhost' in env.hosts:
+        abort('Running push_db on localhost would destroy your database.')
+    run("for table in `psql -Upostgres %s -c'\dt' -A | grep ^public | cut -d'|' -f2`; do psql -Upostgres %s -c\"drop table if exists $table cascade\"; done" % (DB_NAME, DB_NAME,))
+    local("pg_dump -U%s -Fc -Z9 %s | ssh %s@%s pg_restore -U%s -O -x -d %s" % (DB_USERNAME, DB_NAME, SERVER_USERNAME, SERVER_HOSTNAME, DB_USERNAME, DB_NAME,))
+
+
+# Little helpers
+def runserver():
+    with lcd(LOCAL_PROJECT_PATH):
+        with prefix("source deploy/bin/activate"):
+            local("./manage.py runserver")
 
 
 def grep(what):
-    with lcd(PROJECT_PATH):
+    with lcd(LOCAL_PROJECT_PATH):
         local("fgrep -ir %s articles base photos templates wsgi *.py | grep -v 'Binary file .* matches'" % (what,))
+
+
+def new_secretkey():
+    with cd(env.project_path):
+        secretkey = ''.join(random.choice(string.letters + string.digits) for i in xrange(50))
+        sed('local_settings.py', '^SECRET_KEY.*', 'SECRET_KEY = "%s"' % (secretkey,))
+        run("rm settings.py.bak")

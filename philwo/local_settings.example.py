e5cc52c68db5f36768466d6170fa41b8b6232898
Philipp Wollermann <philipp.wollermann@gmail.com>
2012-10-22 23:11:51 +0900

    Update local_settings.example.py

--- philwo/local_settings.example.py
+++ philwo/local_settings.example.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-DEBUG = True
-
 # Administrators
 ADMINS = (
     ('My Name', 'my.email@example'),
@@ -21,20 +19,12 @@ DATABASES = {
 }
 
 # E-Mail Configuration
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.example.com'
-EMAIL_HOST_USER = 'my.email@example'
-EMAIL_HOST_PASSWORD = '1234'
-EMAIL_PORT = 587
 DEFAULT_FROM_EMAIL = 'my.email@example'
 SERVER_EMAIL = DEFAULT_FROM_EMAIL
 SEND_BROKEN_LINK_EMAILS = False
 
-# Internationalization
-TIME_ZONE = 'Asia/Tokyo'
-
 # Disqus API
-DISQUS_FORUM_SHORTNAME = 'philwo'
+DISQUS_FORUM_SHORTNAME = 'example'
 
 # Flickr API
 FLICKR_API_KEY = '123456789123456789123456789'
